d8f32d65791f742b20213e2ba21b2b2d448fc314
Asher Danner <adanner@ets.org>
2021-10-18 13:47:27 -0500

    Initial folder models.py

--- folders/models.py
+++ folders/models.py
@@ -1,9 +1,23 @@
 from django.db import models
 from products.models import Product
+from django.conf import settings
+
 
 # Create your models here.
 class UserFolders(models.Model):
+    user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
     name = models.CharField(max_length=50)
+
+    create_datetime = models.DateTimeField('date created', auto_now_add=True)
+    update_datetime = models.DateTimeField('date updated', auto_now=True)
+
+    class Meta:
+        unique_together = ['user', 'name']
+
+
+class FolderContents(models.Model):
+    folder = models.ForeignKey(UserFolders, on_delete=models.CASCADE)
+    product = models.ForeignKey(Product, on_delete=models.CASCADE)
 
     create_datetime = models.DateTimeField('date created', auto_now_add=True)
     update_datetime = models.DateTimeField('date updated', auto_now=True)
