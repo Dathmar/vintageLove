756556dfda26cdbea7b6ec35b1aec7c3096a8524
Asher Danner <adanner@ets.org>
2021-10-29 13:33:18 -0500

    Rendering of size selection complete.

--- bespokeShipping/forms.py
+++ bespokeShipping/forms.py
@@ -1,10 +1,21 @@
 from django import forms
+from django.utils.safestring import mark_safe
+from django.conf import settings
 
 
-class BespokeShippingForm(forms.ModelForm):
-    size_choices = (('small', 'Small'),
-                    ('medium', 'Medium'),
-                    ('large', 'Large'),
-                    ('xlarge', 'X-Large'))
+class BespokeShippingForm(forms.Form):
+    get_static_prefix = settings.STATIC_URL
+    size_choices = (('small', mark_safe(f'<img src="{ get_static_prefix }img/small.png" height="150"><br>Small')),
+                    ('medium', mark_safe(f'<img src="{ get_static_prefix }img/medium.png" height="150"><br>Medium')),
+                    ('large', mark_safe(f'<img src="{ get_static_prefix }img/large.png" height="150"><br>Large')),
+                    ('xlarge', mark_safe(f'<img src="{ get_static_prefix }img/x-large.png" height="150"><br>X-Large')))
 
-    size = forms.RadioSelect(choices=size_choices, attrs={"onclick: showTab('#list-to-list')"})
+    size = forms.ChoiceField(widget=forms.RadioSelect(attrs={"onclick": "showTab('#list-to-list')",
+                                                             "class": "size-form-check"}),
+                             choices=size_choices)
+
+    first_name = forms.CharField(widget=forms.TextInput(attrs={'placeholder': 'First Name*'}),
+                                 max_length=200)
+    last_name = forms.CharField(widget=forms.TextInput(attrs={'placeholder': 'First Name*'}),
+                                 max_length=200)
+
--- bespokeShipping/views.py
+++ bespokeShipping/views.py
@@ -1,11 +1,16 @@
 from django.shortcuts import render
 from django.contrib.auth.decorators import login_required
+from .forms import BespokeShippingForm
 
 
 # Create your views here.
 @login_required
 def bespoke_shipping_create(request):
-    return render(request, 'bespoke_shipping.html')
+    shipping_form = BespokeShippingForm()
+    context = {
+        'form': shipping_form
+    }
+    return render(request, 'bespoke_shipping.html', context)
 
 
 @login_required
