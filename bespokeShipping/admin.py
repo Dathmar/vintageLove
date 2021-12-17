dd2dde8e21478b5bee9688884b3238ab42e4fec8
Asher Danner <adanner@ets.org>
2021-12-17 13:45:05 -0600

    Updated display and editable fields.

--- bespokeShipping/admin.py
+++ bespokeShipping/admin.py
@@ -5,7 +5,10 @@ from .models import Shipping, ShippingStatus
 # Register your models here.
 @admin.register(Shipping)
 class ShippingAdmin(admin.ModelAdmin):
-    list_display = ['to_name', 'to_email', 'from_name', 'from_email']
+    list_display = ['to_name', 'to_email', 'to_phone', 'from_name', 'from_email', 'status', 'order_window']
+    list_filter = ['status', 'order_window']
+    search_fields = ['to_name', 'to_email', 'to_phone', 'from_name', 'from_email']
+    list_editable = ['status', 'order_window']
 
 
 @admin.register(ShippingStatus)
