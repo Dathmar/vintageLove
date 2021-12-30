e3854efb452ec72ac3f95a3aa8ece7eb9a4a4b34
Asher Danner <asher.danner@gmail.com>
2021-12-30 08:30:57 -0600

    Admin for assigning deliveries

--- deliveries/admin.py
+++ deliveries/admin.py
@@ -1,3 +1,14 @@
 from django.contrib import admin
+from django.db.models import Q
+from .models import Delivery
+
 
 # Register your models here.
+@admin.register(Delivery)
+class DeliveryAdmin(admin.ModelAdmin):
+    list_display = ['user', 'scheduled_date', 'shipping']
+
+    def formfield_for_foreignkey(self, db_field, request=None, **kwargs):
+        if db_field.name == "shipping":
+            kwargs["queryset"] = db_field.related_model.objects.filter(status__name="Picked Up")
+        return super(DeliveryAdmin, self).formfield_for_foreignkey(db_field, request, **kwargs)
