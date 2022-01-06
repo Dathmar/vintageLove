168df228231beb3a1ec34f50bf14616114f92abd
Asher Danner <adanner@ets.org>
2022-01-06 11:12:55 -0600

    Add file upload to shippings.

--- bespokeShipping/admin.py
+++ bespokeShipping/admin.py
@@ -1,8 +1,12 @@
 from django.contrib import admin
-from .models import Shipping, ShippingStatus, Quote
+from .models import Shipping, ShippingStatus, Quote, ShippingFile, ShippingFileType
 
 
 # Register your models here.
+class ShippingFileInline(admin.TabularInline):
+    model = ShippingFile
+
+
 @admin.register(Shipping)
 class ShippingAdmin(admin.ModelAdmin):
     list_display = ['to_name', 'to_email', 'to_phone', 'from_name', 'from_email', 'status', 'order_window']
@@ -10,10 +14,14 @@ class ShippingAdmin(admin.ModelAdmin):
     search_fields = ['to_name', 'to_email', 'to_phone', 'from_name', 'from_email']
     list_editable = ['status', 'order_window']
 
+    inlines = [
+        ShippingFileInline,
+    ]
+
 
 @admin.register(ShippingStatus)
 class ShippingStatusAdmin(admin.ModelAdmin):
     list_display = ['name']
 
-
 admin.site.register(Quote)
+admin.site.register(ShippingFileType)
