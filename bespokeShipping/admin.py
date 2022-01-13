02425e751cb4fa3a8669cd1accf1ffbd4b1ed0fb
Asher Danner <adanner@ets.org>
2022-01-13 16:14:49 -0600

    Add create_datetime to ShippingAdmin

--- bespokeShipping/admin.py
+++ bespokeShipping/admin.py
@@ -9,7 +9,8 @@ class ShippingFileInline(admin.TabularInline):
 
 @admin.register(Shipping)
 class ShippingAdmin(admin.ModelAdmin):
-    list_display = ['to_name', 'to_email', 'to_phone', 'from_name', 'from_email', 'status', 'order_window']
+    list_display = ['to_name', 'to_email', 'to_phone', 'from_name', 'from_email', 'status', 'order_window',
+                    'create_datetime']
     list_filter = ['status', 'order_window']
     search_fields = ['to_name', 'to_email', 'to_phone', 'from_name', 'from_email']
     list_editable = ['status', 'order_window']
