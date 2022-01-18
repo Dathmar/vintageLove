2f7eec18440c617831f77cda4efe9c1f352887e5
Asher Danner <adanner@ets.org>
2022-01-18 10:57:45 -0600

    Add assignment association url

--- deliveries/urls.py
+++ deliveries/urls.py
@@ -5,5 +5,6 @@ from . import views
 app_name = 'deliveries'
 urlpatterns = [
     path('', views.my_assignments, name='my-assignments'),
-    path('assignments/create', views.CreateAssignmentsView.as_view(), name='assignments-create'),
+    path('assignments/create/', views.CreateAssignmentsView.as_view(), name='assignments-create'),
+    path('assignments/associate/', views.AssociateAssignmentsView.as_view(), name='assignments-associate'),
 ]
