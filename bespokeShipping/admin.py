f40f98a907583fb819bdd9ebc925e47e055743bb
Asher Danner <asher.danner@gmail.com>
2022-08-17 16:18:13 -0500

    Add ability to export to CSV for Shippings

--- bespokeShipping/admin.py
+++ bespokeShipping/admin.py
@@ -1,5 +1,30 @@
 from django.contrib import admin
 from .models import Shipping, ShippingStatus, Quote, ShippingFile, ShippingFileType
+import csv
+from django.http import HttpResponse
+import datetime
+
+
+@admin.action(description='Export Selected')
+def export_to_csv(modeladmin, request, queryset):
+    opts = modeladmin.model._meta
+    response = HttpResponse(content_type='text/csv')
+    response['Content-Disposition'] = 'attachment;' 'filename={}.csv'.format(opts.verbose_name)
+    writer = csv.writer(response)
+    fields = [field for field in opts.get_fields() if not field.many_to_many and not field.one_to_many]
+    # Write a first row with header information
+    writer.writerow([field.verbose_name for field in fields])
+    # Write data rows
+    for obj in queryset:
+        data_row = []
+        for field in fields:
+            value = getattr(obj, field.name)
+            if isinstance(value, datetime.datetime):
+                value = value.strftime('%d/%m/%Y')
+            data_row.append(value)
+        writer.writerow(data_row)
+
+    return response
 
 
 # Register your models here.
@@ -22,6 +47,7 @@ class ShippingAdmin(admin.ModelAdmin):
     inlines = [
         ShippingFileInline,
     ]
+    actions = [export_to_csv]
 
 
 @admin.register(ShippingStatus)
